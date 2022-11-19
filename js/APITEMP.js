2614d3bd13880061778754a3387785d1c8ba9d34
CoreyOliver <coreyoliver.dev@gmail.com>
2022-11-19 13:33:19 -0500

    first commit

---
+++ js/APITEMP.js
@@ -0,0 +1,13 @@
+// let searchRes = document.querySelector('input')
+// document.querySelector('button').addEventListener('click', findImage)
+// function findImage() {
+// fetch(`/api/1?q=grass`)
+// .then(res => res.json())
+// .then(data => {
+//    console.log(data)
+// //    document.querySelector('img').src = data[20].imageURL
+// })
+// .catch(err => {
+//     console.log(`error ${err}`)
+// });
+// }
